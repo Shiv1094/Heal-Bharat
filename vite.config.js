3cc99123f79372452d19bba143b8b962167e5ffb
Shiv1094 <shiv1094bhandekari@gmail.com>
2025-08-26 17:17:51 +0530

    setup gh-pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "/Heal-Bharat-Task-2-Tours-and-Travel-Website/",  // must match your repo name
   plugins: [react()],
 })
